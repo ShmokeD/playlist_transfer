45cdb1712e92c813a0b94a5d270ac65714e382d4
Omkesh Dwivedi <omkeshdwivedi@gmail.com>
2021-04-11 15:02:29 +0530

    Remove unnecessary files

--- lib/apis/spotify_api/spotify_api_objects/spotify_external_url.dart
+++
@@ -1,8 +0,0 @@
-class SpotifyExternalUrl {
-  SpotifyExternalUrl.fromJson(Map<String, dynamic> json) {
-    spotifyUrl = json['spotify'] ?? 'null url returned';
-  }
-
-  String spotifyUrl;
-}
-/* Thats it thats the ExternalUrlObject that spotfy returns */
